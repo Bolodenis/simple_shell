577fc41dec1237d7bb2ef37aa5aeba762002bb1b
root <bolodenis>
2023-10-17 11:31:17 -0700

    adding changes

--- get_token.c
+++ get_token.c
@@ -1,20 +1,8 @@
 #include "team.h"
 /**
  * parse_input - Tokenize a string into an array of arguments.
- * @lineptr: The input string to be tokenized.
- * @args: An array of strings to store the tokenized arguments.
- *
- * Description:
- * This function takes an input string `lineptr` and tokenize it into individal
- * arguments, which are stored in the `args` array. It use whitespace characers
- * such as space, tab, and newline, as delimiters to separate the arguments.
- *
- * Parameters:
- *   lineptr: The input string to be tokenized.
- *   args: An array of strings to store the tokenized arguments.
- *
- * Return: No return value, but it populates the `args`
- * array with the tokenized arguments.
+ * @buf: The input string to be tokenized.
+ * @args: Array to store tokenized arguments.
  */
 void parse_input(char *buf, char *args[])
 {
